71d3f8060b2f5e15bc328b289cb4d177fdce5507
Paul Q <pauldquinones@gmail.com>
2018-09-24 22:40:22 -0400

    Comments added

--- src/JSONGetter.java
+++ src/JSONGetter.java
@@ -8,19 +8,23 @@ import java.util.Scanner;
 public class JSONGetter {
     public static JSONObject GetJsonString(String urlText){
         try {
-            URL url = new URL(urlText);
+            URL url = new URL(urlText);//set url test and grab url
 
-            Scanner scan = new Scanner(url.openStream());
+            Scanner scan = new Scanner(url.openStream());//open url stream
 
-            String jsonString = new String();
+            String jsonString = new String();//string to hold json text
+
+            //loop through the stream and push to json string
             while (scan.hasNext())
                 jsonString += scan.nextLine();
-            scan.close();
 
+            scan.close();//close scan connection
+
+            //check if json array and convert to a json object
             if(jsonString.charAt(0) == '[' && jsonString.charAt(jsonString.length() - 1) == ']')
                 jsonString = "{jsonObject:" + jsonString + "}";
 
-            return new JSONObject(jsonString);
+            return new JSONObject(jsonString);//return json object
         } catch (MalformedURLException e){
             return new JSONObject();//send empty jsonObject
         } catch (IOException e){
